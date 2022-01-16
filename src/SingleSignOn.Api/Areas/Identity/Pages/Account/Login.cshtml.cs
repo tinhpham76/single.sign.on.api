6ee4e79f9d0afdd73f3a79acb5288a9ea544e529
tinhpv3 <tinhpv3@fpt.com.vn>
2022-01-16 14:57:58 +0700

    fix

--- src/SingleSignOn.Api/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/SingleSignOn.Api/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace SingleSignOn.Api.Areas.Identity.Pages.Account
 {
@@ -97,6 +99,43 @@ namespace SingleSignOn.Api.Areas.Identity.Pages.Account
                     return Page();
                 }
             }
+
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Role, "Administrator"),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            //AllowRefresh = <bool>,
+            // Refreshing the authentication session should be allowed.
+
+            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+            // The time at which the authentication ticket expires. A 
+            // value set here overrides the ExpireTimeSpan option of 
+            // CookieAuthenticationOptions set with AddCookie.
+
+            //IsPersistent = true,
+            // Whether the authentication session is persisted across 
+            // multiple requests. When used with cookies, controls
+            // whether the cookie's lifetime is absolute (matching the
+            // lifetime of the authentication ticket) or session-based.
+
+            //IssuedUtc = <DateTimeOffset>,
+            // The time at which the authentication ticket was issued.
+
+            //RedirectUri = <string>
+            // The full path or absolute URI to be used as an http 
+            // redirect response value.
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme, 
+            new ClaimsPrincipal(claimsIdentity), 
+            authProperties);
 
             // If we got this far, something failed, redisplay form
             return Page();
